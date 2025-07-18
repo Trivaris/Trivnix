fc134f0ac4d3e62f1dfaa11dbd01a59e21405c45
trivaris <github@tripple.lurdane.de>
2025-07-19 00:55:00 +0200

    fix: Fixed Proxy HTTP Headers

--- hosts/modules/reverse-proxy.nix
+++ hosts/modules/reverse-proxy.nix
@@ -66,8 +66,8 @@ in
           proxyWebsockets = true;
           extraConfig = ''
             proxy_set_header Upgrade $http_upgrade;
-            proxy_set_header Host $http_host;
-            proxy_set_header Connection upgrade;
+            proxy_set_header Host $host;
+            proxy_set_header Connection $connection_upgrade;
             proxy_set_header Accept-Encoding gzip;
             proxy_set_header X-Real-IP $http_cf_connecting_ip;
             proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
