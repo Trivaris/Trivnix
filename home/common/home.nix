299078bef919fbe2a3aadbc7beffcd19642cdb70
trivaris <github@tripple.lurdane.de>
2025-08-15 03:48:23 +0200

    fix: Removed rmClobbering system service

--- home/common/home.nix
+++ home/common/home.nix
@@ -1,6 +1,5 @@
 {
   outputs,
-  pkgs,
   lib,
   trivnixLib,
   hostInfos,
@@ -11,30 +10,6 @@
   home.username = lib.mkDefault userInfos.name;
   home.homeDirectory = lib.mkDefault "/home/${userInfos.name}";
   home.stateVersion = hostInfos.stateVersion;
-
-  systemd.user.services.rmClobbering = {
-    Unit = {
-      Description = "Remove unwanted backup files";
-      After = [ "default.target" ];
-    };
-    Service = {
-      Type = "oneshot";
-      ExecStart = pkgs.writeShellScript "rm-clobbering" ''
-        #!/usr/bin/env bash
-        rm -f ~/.gtkrc-2.0.backup
-        rm -f ~/.librewolf/${userInfos.name}/search.json.mozlz4.backup
-        rm -f ~/.config/gtk-3.0/gtk.css.backup 2>/dev/null || true
-      '';
-    };
-    Install = {
-      WantedBy = [ "default.target" ];
-    };
-  };
-
-  home.activation.runRmClobberingService =
-    lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      /run/current-system/sw/bin/systemctl --user start rmClobbering.service || true
-    '';
 
   nixpkgs = {
     overlays = builtins.attrValues (outputs.overlays);
