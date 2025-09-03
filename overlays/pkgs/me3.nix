35f557532d08d35c27f5214eb1f14f163081d32c
trivaris <github@tripple.lurdane.de>
2025-09-04 00:00:16 +0200

    fix: Added libG

--- overlays/pkgs/me3.nix
+++ overlays/pkgs/me3.nix
@@ -10,19 +10,33 @@ let
     };
 
     sourceRoot = ".";
-    nativeBuildInputs = [ pkgs.autoPatchelfHook ];
-    buildInputs = [ pkgs.glibc pkgs.gcc.cc.lib ];
+    nativeBuildInputs = [ pkgs.autoPatchelfHook pkgs.makeWrapper ];
+    buildInputs = builtins.attrValues {
+      inherit (pkgs.xorg) libX11;
+      inherit (pkgs.gcc.cc) lib;
+      inherit (pkgs)
+        glibc 
+        libGL
+        zlib;
+    };
 
     installPhase = ''
+      runHook preInstall
       mkdir -p $out/share/me3
-      cp -r * $out/share/me3
+      cp -r . $out/share/me3
 
       mkdir -p $out/bin
-      ln -s $out/share/me3/bin/me3 $out/bin/me3
 
+      # wrap with steam-run so Steam/Proton/sniper find GL & friends
+      makeWrapper ${pkgs.steam-run}/bin/steam-run $out/bin/me3 \
+        --add-flags "$out/share/me3/bin/me3"
+
+      # expose the helper launchers via steam-run as well
       for f in launch-*.sh; do
-        ln -s $out/share/me3/$f $out/bin/$f
+        makeWrapper ${pkgs.steam-run}/bin/steam-run $out/bin/$f \
+          --add-flags "$out/share/me3/$f"
       done
+      runHook postInstall
     '';
 
     meta = {
