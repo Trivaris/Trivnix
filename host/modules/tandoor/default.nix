63bc6ee7a00bf03a8ba3f219faa23831e04684c7
trivaris <git@trivaris.org>
2025-11-30 22:08:27 +0100

    update: inherit

--- host/modules/tandoor/default.nix
+++ host/modules/tandoor/default.nix
@@ -5,8 +5,8 @@ in
 {
   config = lib.mkIf prefs.tandoor.enable {
     services.tandoor-recipes = {
+      inherit (prefs.tandoor.reverseProxy) port;
       enable = true;
-      port = prefs.tandoor.reverseProxy.port;
       address = prefs.tandoor.reverseProxy.domain;
     };
   };
