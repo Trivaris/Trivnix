6b5c20f9d54ed8b9648ccfad30a355e84b21828f
trivaris <git@trivaris.org>
2026-06-08 09:56:27 +0200

    feat: sum sum

--- host/modules/cfddns/default.nix
+++ host/modules/cfddns/default.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 let
@@ -8,9 +9,15 @@ let
 in
 {
   config = lib.mkIf cfddnsPrefs.enable {
-    services.cfddns = {
-      inherit (cfddnsPrefs.reverseProxy) port;
-      enable = true;
+    systemd.services.cfddns = {
+      description = "Cloudflare Dyn DNS Middleware Server";
+      wantedBy = [ "multi-user.target" ];
+      after = [ "network.target" ];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${lib.getExe pkgs.cfddns-middleware} --port ${toString cfddnsPrefs.reverseProxy.port}";
+        Restart = "on-failure";
+      };
     };
   };
 }
