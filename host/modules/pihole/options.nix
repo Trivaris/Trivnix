443b6e7290334eaa4b61b22973f79af3cbace164
trivaris <git@trivaris.org>
2026-06-08 09:24:33 +0200

    feat: sum sum

--- host/modules/pihole/options.nix
+++ host/modules/pihole/options.nix
@@ -6,15 +6,15 @@
   options.hostPrefs.piHole = {
     enable = lib.mkEnableOption "Pi Hole, a DNS Sink";
     reverseProxy = lib.mkReverseProxyOption;
-    lists = lib.mkOption {
-      type = lib.types.listOf (lib.types.submodule {
-        options = {
-          enabled = lib.mkEnableOption "The Adlist";
-          description = lib.mkOption { type = lib.types.str; };
-          type = lib.mkOption { type = lib.types.enum [ "allow" "block" ]; };
-          url = lib.mkOption { type = lib.types.str; };
-        };
-      });
-    };
+    # lists = lib.mkOption {
+    #   type = lib.types.listOf (lib.types.submodule {
+    #     options = {
+    #       enabled = lib.mkEnableOption "The Adlist";
+    #       description = lib.mkOption { type = lib.types.str; };
+    #       type = lib.mkOption { type = lib.types.enum [ "allow" "block" ]; };
+    #       url = lib.mkOption { type = lib.types.str; };
+    #     };
+    #   });
+    # };
   };
 }
