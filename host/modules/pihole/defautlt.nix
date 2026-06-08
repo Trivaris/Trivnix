b8a07299cb9c21427b9280f7df9edcb8d796dfa3
trivaris <git@trivaris.org>
2026-06-08 09:33:42 +0200

    feat: sum sum

--- host/modules/pihole/defautlt.nix
+++ host/modules/pihole/defautlt.nix
@@ -35,27 +35,27 @@ in
         ports = [ (if piHolePrefs.reverseProxy.enable then piHolePrefs.reverseProxy.port else 80) ];
       };
 
-      nginx.appendConfig = lib.mkIf piHolePrefs.reverseProxy.enable ''
-        stream {
-            upstream pihole_backend {
-                server ${wgIp}:53;
-            }
-       
-            server {
-                listen 853 ssl;
-                proxy_pass pihole_backend;
-      
-                ssl_certificate /var/lib/acme/${piHolePrefs.reverseProxy.domain}/fullchain.pem;
-                ssl_certificate_key /var/lib/acme/${piHolePrefs.reverseProxy.domain}/key.pem;
-      
-                ssl_protocols TLSv1.2 TLSv1.3;
-                ssl_ciphers HIGH:!aNULL:!MD5;
-                ssl_handshake_timeout 10s;
-                ssl_session_cache shared:SSL:10m;
-                ssl_session_timeout 3h;
-            }
-        }
-      '';
+      # nginx.appendConfig = lib.mkIf piHolePrefs.reverseProxy.enable ''
+      #   stream {
+      #       upstream pihole_backend {
+      #           server ${wgIp}:53;
+      #       }
+      #  
+      #       server {
+      #           listen 853 ssl;
+      #           proxy_pass pihole_backend;
+      # 
+      #           ssl_certificate /var/lib/acme/${piHolePrefs.reverseProxy.domain}/fullchain.pem;
+      #           ssl_certificate_key /var/lib/acme/${piHolePrefs.reverseProxy.domain}/key.pem;
+      # 
+      #           ssl_protocols TLSv1.2 TLSv1.3;
+      #           ssl_ciphers HIGH:!aNULL:!MD5;
+      #           ssl_handshake_timeout 10s;
+      #           ssl_session_cache shared:SSL:10m;
+      #           ssl_session_timeout 3h;
+      #       }
+      #   }
+      # '';
     };
     
     networking.firewall.interfaces."${wireguardPrefs.interfaceName}" = {
