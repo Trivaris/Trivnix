cbaa0af03b5c87664b3fe12c6a83343407a2e480
trivaris <git@trivaris.org>
2026-06-10 20:41:34 +0200

    feat: feet

--- host/modules/homeAssistant/default.nix
+++ host/modules/homeAssistant/default.nix
@@ -13,16 +13,12 @@ in
       openFirewall = !homeAssistantPrefs.reverseProxy.enable;
       configWritable = true;
       extraComponents = homeAssistantPrefs.extraComponents;
-      # config = {
-      #   http = {
-      #     use_x_forwarded_for = true;
-      #     trusted_proxies = [ "127.0.0.1" ];
-      #     server_port = homeAssistantPrefs.reverseProxy.port;
-      #     server_host =
-      #       [ "127.0.0.1" ]
-      #       ++ (lib.optionals homeAssistantPrefs.wireguard.enable [ "10.0.0.1" ]);
-      #   };
-      # };
+      config = {
+        http = {
+          use_x_forwarded_for = true;
+          trusted_proxies = [ "127.0.0.1" "::1" ]; 
+        };
+      };
     };
   };
 }
