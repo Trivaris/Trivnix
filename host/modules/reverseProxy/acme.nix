85a43ea2c89c203a979a6012324f98072f38e621
trivaris <git@trivaris.org>
2026-09-06 02:15:16 +0200

    feet: feet

--- host/modules/reverseProxy/acme.nix
+++ host/modules/reverseProxy/acme.nix
@@ -20,7 +20,7 @@ in
       certs = builtins.listToAttrs (
         [ (lib.nameValuePair reverseProxyPrefs.zone { }) ]
         ++ (map (service: lib.nameValuePair service.domain { }) config.vars.activeServices)
-        ++ (map (extraCertDomain: lib.nameValuePair extraCertDomain { }) (reverseProxyPrefs.extraCertDomains ++ [ reverseProxyPrefs.anubisAuthDomain ]))
+        ++ (map (extraCertDomain: lib.nameValuePair extraCertDomain { }) reverseProxyPrefs.extraCertDomains)
       );
     };
 
