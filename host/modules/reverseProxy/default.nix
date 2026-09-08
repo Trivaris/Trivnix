57afa07d7dab636968386186b6baff2d21f71f92
trivaris <git@trivaris.org>
2026-09-08 21:46:04 +0200

    feet: feet

--- host/modules/reverseProxy/default.nix
+++ host/modules/reverseProxy/default.nix
@@ -3,7 +3,7 @@ let
   reverseProxyPrefs = config.hostPrefs.reverseProxy;
   allowedTCPPorts = (map (
     service: service.externalPort
-  ) config.vars.activeServices) ++ lib.optionals reverseProxyPrefs.dumbPipes.enable (map (tcpForward: tcpForward.listenPort) reverseProxyPrefs.dumbPipes.tcpForwards);
+  ) config.vars.activeServices) ++ lib.optionals reverseProxyPrefs.dumbPipes.enable (map (tcpForward: tcpForward.listenPort) (builtins.attrValues reverseProxyPrefs.dumbPipes.tcpForwards));
 in
 {
   config = lib.mkIf reverseProxyPrefs.enable {
